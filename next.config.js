4dfd2b1336d5b58693ddaf6cfa3f21d3816238b8
Be Major <bemajor@bes-imac.localdomain>
2022-01-13 20:28:58 +0100

    info animation

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
+}
+
+
+module.exports = (phase, {defaultConfig}) => {
+  if ('sassOptions' in defaultConfig) {
+      defaultConfig['sassOptions'] = {
+          includePaths: [path.join(__dirname, 'styles')],
+          prependData: `@import "vars.scss";`,
+      }
+  }
+  return defaultConfig;
 }
