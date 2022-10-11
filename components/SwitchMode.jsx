0d85b6b40d3e92b7b6de8bf63bb7404e297794ff
bernardbolter <bernardbolter@gmail.com>
2022-10-11 22:46:08 +0200

    switch mode more

--- components/SwitchMode.jsx
+++ components/SwitchMode.jsx
@@ -1,23 +1,22 @@
 import React, { useContext } from 'react'
 import { ArtContext } from '../providers/ArtProvider'
-import SwitchModeStyle from '../styles/switch-mode.style'
-import { useTheme } from 'styled-components'
+import SwitchModeStyle from '../styles/switch-mode.style.js'
+import { motion } from 'framer-motion'
 
 const SwitchMode = () => {
     const [art, setArt] = useContext(ArtContext)
-    const theme = useTheme()
-    console.log("switch theme: ", theme)
-
-    const onToggle = () => setArt(state => ({ ...state, theme: state.theme === 'light' ? 'dark' : 'light' }))
 
     return (
         <SwitchModeStyle>
-            <div className="switchmode-container">
-                <label>
-                    <input type="checkbox" checked={art.theme ==='light'} onChange={onToggle} />
-                    <span />
-                </label>
-                <p>switch to mode</p>
+            <div 
+                className="switchmode-container"
+                onClick={() => setArt(state => ({ ...state, theme: state.theme === 'light' ? 'dark' : 'light' }))}
+            >
+                <motion.p>{art.theme === 'dark' && 'light'}</motion.p>
+                    <motion.span 
+
+                    />
+                <motion.p>{art.theme === 'light' && 'dark'}</motion.p>
             </div>
         </SwitchModeStyle>
     )
--- styles/switch-mode.style.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const SwitchModeStyle = styled.div`
-    switchmode-container {
-        display: block
-    }
-`
-
-export default SwitchModeStyle
